51a84f293d50d222e8cd632dccbb02ad2a4c5bcb
Emerson Joaquim Tito Albino <ealbino@c2r4s7.42luanda.com>
2026-05-12 13:01:02 +0100

    Rename menu for List Users instead of List Teachers

--- menus/user_menu.py
+++ menus/user_menu.py
@@ -3,7 +3,7 @@ from menus.report_menu import report_menu
 def user_menu(role: str):
     if role == "coordinator":
         print("1 - Register User")
-        print("2 - List Teacher")
+        print("2 - List Users")
         print("3 - Reports")
         print("0 - logout")
     elif role == "teacher":
